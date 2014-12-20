3ce61824a64f63cefa719d8feef0130e9fa4174e
moljac <mcvjetko@holisticware.net>
2014-12-20 17:25:34 +0100

    script for download and git submodule checkout

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+
+
+all:		download
+
+download:	
---
+++ pj/initialize-donwload-pjsip.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+SRC_PJSIP_BZ2=pjproject-2.3.tar.bz2
+SRC_PJSIP_ZIP=pjproject-2.3.zip
+
+
+
+
+rm -fr pjproject-*.tar.bz2 
+rm -fr pjproject-*.zip 
+
+ls -al
+
+curl -LOk http://www.pjsip.org/release/2.3/$SRC_PJSIP_BZ2
+curl -LOk http://www.pjsip.org/release/2.3/$SRC_PJSIP_ZIP
+
+ls -al
+
+unzip 		$SRC_PJSIP_ZIP
+tar xvfj	$SRC_PJSIP_BZ2
+
+cd ../submodulez/
+rm -fr ./PJsip-CSharp/
+rm -fr ./pjsip4net/
+rm -fr ./swig/
+
+git submodule update --init --recursive
+
+
+cd swig/
